4aaa6d8cd93579c76925943518cb470e1f32e6b7
arnnvv <arnav.900@protonmail.com>
2024-05-10 12:29:36 +0530

    connection

--- index.ts
+++ index.ts
@@ -1,25 +1,19 @@
-import WebSocket, { WebSocketServer } from "ws";
+Bun.serve({
+  port: 3000,
+  fetch(request, server) {
+    if (server.upgrade(request)) return;
 
-const port: number = 8080;
-
-const server = Bun.serve({
-  port: port,
-  fetch(req) {
-    console.log(`${new Date()} Server listning on ${req.url}`);
-    return new Response("HII this is a bun server");
+    return new Response("Hello World");
   },
-});
-
-const wss = new WebSocketServer({ server });
-
-wss.on("connection", (ws) => {
-  ws.on("error", console.error);
-  ws.on("message", (data, isBinary) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
-  });
-  ws.send("Hello World!");
+  websocket: {
+    open(ws) {
+      console.log(`Websocket opened ${ws}`);
+    },
+    message(ws, message) {
+      console.log(`Websocket message ${ws} ${message}`);
+    },
+    close(ws) {
+      console.log(`Websocket closed ${ws}`);
+    },
+  },
 });
