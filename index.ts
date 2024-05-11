65ef9e60fc8c7ba7d01d27274207a16199146e00
arnnvv <arnav.900@protonmail.com>
2024-05-11 18:41:39 +0530

    time]

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-Bun.serve({
+const server = Bun.serve({
   port: 3000,
   fetch(request, server) {
     if (server.upgrade(request)) return;
@@ -7,13 +7,20 @@ Bun.serve({
   },
   websocket: {
     open(ws) {
-      console.log(`Websocket opened ${ws}`);
+      const welcomeMessage =
+        "Welcome ask me the time and I'll tell you what time it is";
+      console.log(`Websocket opened ${ws} ${welcomeMessage}`);
     },
     message(ws, message) {
-      console.log(`Websocket message ${ws} ${message}`);
+      console.log(`Incomming message ${ws} ${message}`);
+      if (message.trim().toLowerCase() === "time") {
+        const time = new Date().toLocaleTimeString();
+      }
     },
     close(ws) {
       console.log(`Websocket closed ${ws}`);
     },
   },
 });
+
+console.log(`Listening on ${server.url}`);
